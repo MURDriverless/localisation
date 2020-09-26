8054f6d8452d47978a2e6727299361141014a968
Michelle <shellyk1997@gmail.com>
2020-09-26 15:23:28 +1000

    Initialisation
    Tortoisebot requires further tuning to match sim speed. mursim compatibility still wip

--- README.md
+++ README.md
@@ -1,1 +1,132 @@
-localisation
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+
+  <h3 align="center"> WIP Localisation </h3>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#about-the-project)
+  * [Built With](#built-with)
+* [TortoiseBot](#tortoisebot)
+  * [Requirements](#requirements)
+  * [Installation](#installation)
+* [MURSIM](#mursim)
+  * [Requirements](#requirements)
+  * [Installation](#installation)
+* [Folder Structures](#folder-structures)
+* [Todo](#todo)
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+Developed for the 2020 Melbourne University Racing (MUR) Motorsports Driverless Project as part of the Spatial and Perception System for the Vehicle State Estimation Subsystem. Multiple rospy files are within this repo for different purposes, including one that was developed using mobile phone sensors. The most recent is `tortoisebotlistener.py` which is detailed throughout this README.md file. This is designed to filter odometry data from the `tortoisebot` simulation by using Kalman Filters. 
+
+<!-- TORTOISEBOT -->
+## Tortoisebot
+
+### Requirements
+
+* tortoisebot
+* hector_gazebo
+* common_msgs
+* ouster_example
+* ROS melodic
+
+### Installation
+0. Install 'sudo apt install ros-melodic-desktop-full' and 'sudo apt install python-catkin-tools'
+1. Clone the repo to an existing ROS Workspace (use catkin build)
+```sh
+git clone https://github.com/mfkeane/avse.git
+```
+2. Clone required repos to the same ROS Workspace
+```sh
+git clone https://github.com/mfkeane/common_msgs.git
+git clone https://github.com/mfkeane/tortoisebot.git
+git clone https://github.com/stevenlee090/ouster_example.git
+git clone https://github.com/tu-darmstadt-ros-pkg/hector_gazebo.git
+```
+3. Install required libraries, mainly `teleop_twist_keyboard` for controlling the robot movement.
+```
+sudo apt-get install ros-melodic-teleop-twist-keyboard
+```
+3. Build the ROS Workspace with catkin build
+4. Source the ROS Workspace
+```sh
+source devel/setup.bash
+```
+5. From the ROS Workspace, which will open Gazebo and RViz:
+```sh
+roslaunch tortoisebot tortoisebot.launch model:="$(find tortoisebot)/urdf/tortoisebot.urdf.xacrp"
+```
+6. From a seperate terminal:
+```sh
+rosrun localisation tortoisebotlocalisation.py
+```
+7. To control the robot, run this from another seperate terminal:
+```sh
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+```
+8. In RViz, add the `RobotModel` display and the two odometry messages, `/odom` and `/mur/Odom`. The first is the raw noisy readings from the GPS, and the second is the filtered data from the GPS and IMU. Change the colour of these to differientate them. Set fixed frame to `odom` and uncheck any covariances. You may wish to set to TopDownOrtho view and reduce the tolerances and keep variables for clarity.
+
+Once finished, you should end up with something like this\
+![demo](assets/demo.gif)
+
+<!-- MURSIM -->
+## MURSIM
+
+### Requirements
+
+* mur_sim
+* See full list under the mur_sim repo
+
+### Installation
+WIP
+0. Install 'sudo apt install ros-melodic-desktop-full' and 'sudo apt install python-catkin-tools'
+1. Setup ROS Workspace as per mur_sim readme
+2. Clone this repo to the MURSIM existing ROS Workspace (use catkin build)
+```sh
+git clone https://github.com/MURDriverless/localisation.git
+```
+3. Build the ROS Workspace with catkin build
+4. Source the ROS Workspace
+```sh
+source devel/setup.bash
+```
+5. From the ROS Workspace, launch mur_sim
+6. From a seperate terminal:
+```sh
+rosrun avse tortoisebotlistener.py
+```
+7. To control the robot, run this from another seperate terminal:
+```sh
+rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+```
+or use rqt
+8. In RViz, add the `RobotModel` display and the two odometry messages, `/odom` and `/mur/Odom`. The first is the raw noisy readings from the GPS, and the second is the filtered data from the GPS and IMU. Change the colour of these to differientate them. Set fixed frame to `odom` and uncheck any covariances. You may wish to set to TopDownOrtho view and reduce the tolerances and keep variables for clarity.
+
+
+## Folder Structures
+
+```
+├── localisation
+│   ├── README.md
+|   └── localisation
+|       ├── CMakeLists.txt
+│       ├── package.xml
+│       ├── setup.py
+|       └── src
+|           ├── __init__.py
+|           └── scripts
+|               ├── mursimlocalisation.py
+|               └── tortoisebotlocalisation.py
+```
+
+## Todo
+- [x] Further tune the Kalman Filters
+- [x] MURSIM compatibility
---
+++ localisation/src/scripts/mursimlocalisation.py
@@ -0,0 +1,360 @@
+#!/usr/bin/env python
+import rospy
+import message_filters
+import math
+from std_msgs.msg import String, Bool
+from geometry_msgs.msg import Vector3, Quaternion, PoseWithCovariance, TwistWithCovariance, Pose, Point, Twist, Vector3Stamped
+from sensor_msgs.msg import NavSatFix, Imu, MagneticField
+from nav_msgs.msg import Odometry
+
+import utm
+
+import numpy as np
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from sympy import Symbol, symbols, Matrix, sin, cos
+from sympy import init_printing
+from sympy.utilities.codegen import codegen
+init_printing(use_latex=True)
+
+
+
+#-----------------------------------------------------------
+
+class Server:
+    def __init__(self):
+        self.gps = None
+        self.imu = None
+        self.mf = None
+        self.gps_velocity = None
+
+        self.past_gps = None
+        self.past_imu = None
+        self.past_mf = None
+
+	self.calibrated = False
+
+        self.D = 11.824 # Magnetic Declination (Mornington)
+
+        self.firstimu = 1
+        self.headingimu = 0.0
+        self.firstheadingimu = 0.0
+        self.angular_velocity = 0.0
+
+	self.firstodom = 1
+        self.headingodom = 0.0
+        self.firstheadingodom = 0.0
+        self.angular_velocity_gps = 0.0
+        
+        self.velocity = Vector3(x=0.0,y=0.0,z=0.0)
+        self.k_velocity = Vector3(x=0.0,y=0.0,z=0.0)
+        self.position = Vector3(x=0.0,y=0.0,z=0.0)
+        self.k_position = Vector3(x=0.0,y=0.0,z=0.0)
+        self.acceleration = Vector3(x=0.0,y=0.0,z=0.0)
+
+	self.firstgps = 0
+	self.firstposition = [0,0]
+        self.past_headingimu = []
+        self.past_headinggps = []
+        self.past_acceleration_x = []
+        self.past_acceleration_y = []
+        self.past_acceleration_z = []
+        self.past_velocity_x = []
+        self.past_velocity_y = []
+        self.past_velocity_z = []
+        self.past_position_x = []
+        self.past_position_y = []
+        self.past_position_z = []
+	
+        self.dx = 0
+        self.dy = 0
+	
+        self.x_k = np.array([[0],[0],[0],[0]])
+
+        self.acc_k = np.array([[0],[0],[0],[0]])
+
+        self.kf_past_velocity_x = []
+        self.kf_past_velocity_y = []
+        self.kf_past_position_x = []
+        self.kf_past_position_y = []
+        self.P = np.array([[0.01,0,0,0],[0,0.64,0,0],[0,0,0.01,0],[0,0,0,0.01]])
+
+        self.accP = np.array([[0.01,0,0,0],[0,0.1,0,0],[0,0,0.01,0],[0,0,0,0.1]])
+
+        self.start_time = 0.0
+        self.time = 0.0
+        self.dt = 0.02
+        self.gpsdt = 0.0
+        self.gpstime = 0.0
+        self.imudt = 0.0
+        self.imutime = 0.0
+        self.distance = 0.0
+
+        self.pitchimu = 0.0;
+        self.rollimu = 0.0;
+        self.yawimu =  0.0;
+        self.pitchgps = 0.0;
+        self.rollgps = 0.0;
+        self.yawgps = 0.0;
+
+        self.numstates = 4
+
+    def quaternion_from_euler(self, roll, pitch, yaw):
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        return Quaternion(x=qx, y=qy, z=qz, w=qw)
+
+    def pi_2_pi(self, angle):
+        return (angle + np.pi)%(2*np.pi)-np.pi
+
+    def kalman(self):
+        # Update time rate
+        delta_t = self.gpsdt
+        current_time = rospy.Time.now()
+
+        if self.x_k[3] == 0.:
+            self.start_time = current_time
+	
+        # POSITION AND VELOCITY
+        # Process
+        xk_1 = self.x_k # [pos_x, pos_y, vel_x, vel_y]
+        ak_1 = np.array([[self.acceleration.x], [self.acceleration.y]])
+        A = np.array([[1,0,delta_t,0],[0,1,0,delta_t],[0,0,1,0],[0,0,0,1]])
+        B = np.array([[0.5*(delta_t*delta_t),0],[0,0.5*(delta_t*delta_t)],[delta_t,0],[0,delta_t]])
+
+        # Measurements
+        zk = np.array([[self.position.x], [self.position.y], [self.velocity.x],[self.velocity.y]])
+        H = np.array([[1,0,delta_t,0],[0,1,0,delta_t],[0,0,1,0],[0,0,0,1]])
+
+        # Error Matrices
+        # Disturbance Covariances (model)
+        Q = np.array([[0.05,0.025,0,0],[0.025,0.025,0,0],[0,0,0.05,0.025],[0,0,0.025,0.025]])
+        # Noise Covariances (Sensors)
+        R = np.array([[20,0,0,0],[0,20,0,0],[0,0,2,0],[0,0,0,2]])
+
+
+        # Prediction Equations
+        # State Prediction X = Ax + Bu
+        X_k = A.dot(xk_1) + B.dot(ak_1)
+        # Covariance Prediction
+        p = A.dot(self.P).dot(A.transpose()) + Q
+	
+        # Update Equations
+        # Kalman Gain
+        K = p.dot(H.transpose()).dot(np.linalg.inv(H.dot(p).dot(H.transpose())+R))
+        # State Update x_(n,n-1) + K_n*(z_n - x_(n,n-1))
+        self.x_k = X_k + K.dot(zk - H.dot(X_k))
+        # Covariance Update (1-K_n)p_(n,n-1)
+        self.P = (np.identity(4)-K.dot(H)).dot(p)
+        self.kf_past_position_x.append(self.x_k[0])
+        self.kf_past_position_y.append(self.x_k[1])
+        self.kf_past_velocity_x.append(self.x_k[2])
+        self.kf_past_velocity_y.append(self.x_k[3])
+        self.kf_position = (self.x_k[0],self.x_k[1],0.0)
+
+        #ORIENTATION
+        # Process
+        acck_1 = self.acc_k # [theta; bias; thetagps;biasgps]
+        qk_1 = np.array([[self.angular_velocity],[self.angular_velocity_gps]])
+        Aacc = np.array([[1,-self.imudt,0,0],[0,1,0,0],[0,0,1,-delta_t],[0,0,0,1]])
+        Bacc = np.array([[self.imudt,0],[0,0],[0,delta_t],[0,0]])
+
+        # Measurements
+        zk = np.array([[self.pi_2_pi(self.yawimu)], [self.pi_2_pi(self.yawgps)]])
+        H = np.array([[1,0,0,0],[0,0,1,0]])
+
+        # Error Matrices
+        # Disturbance Covariances (model)
+        Q = np.array([[np.power(self.imudt,2)*1,0,0,0],[0,1,0,0],[0,0,np.power(delta_t,2)*1,0],[0,0,0,1]])
+        # Noise Covariances (Sensors)
+        R = np.array([[0.01,0],[0,1]])
+
+
+        # Prediction Equations
+        # State Prediction X = Ax + Bu
+        X_k = self.pi_2_pi(Aacc.dot(acck_1) + Bacc.dot(qk_1))
+        # Covariance Prediction
+        p = Aacc.dot(self.accP).dot(Aacc.transpose()) + Q
+
+        # Update Equations
+        # Kalman Gain
+        K = p.dot(H.transpose()).dot(np.linalg.pinv(H.dot(p).dot(H.transpose())+R))
+        # State Update x_(n,n-1) + K_n*(z_n - x_(n,n-1))
+        self.acc_k = X_k + K.dot(self.pi_2_pi(zk - H.dot(X_k)))
+        self.acc_k[0]=self.pi_2_pi(self.acc_k[0])
+        self.acc_k[2]=self.pi_2_pi(self.acc_k[2])
+        # Covariance Update (1-K_n)p_(n,n-1)
+        self.accP = (np.identity(4)-K.dot(H)).dot(p)
+
+        # 30 seconds of calibration before vehicle starts
+        calibration_bool_pub = rospy.Publisher('/mur/CalibratingGPS', Bool, queue_size=10)
+        calibration = Bool()
+        if (current_time.to_sec() - self.start_time.to_sec()) > 10.0:
+            calibration.data = True
+            self.calibrated = True
+        else:
+            calibration.data = False
+        calibration_bool_pub.publish(calibration)
+
+        odom_pub = rospy.Publisher('/mur/Odom', Odometry, queue_size=10)
+
+        odom = Odometry()
+        odom.header.stamp = current_time
+        odom.header.frame_id = "odom"
+
+        if self.x_k[3] == 0.:
+	    odom.pose.pose = Pose(Point(self.x_k[0],self.x_k[1], 0.), self.quaternion_from_euler(0.,0.,0.))
+        else:
+            odom.pose.pose = Pose(Point(self.x_k[0],self.x_k[1], 0.), self.quaternion_from_euler(0,0,self.pi_2_pi(self.acc_k[0])))
+        odom.child_frame_id = "base_link"
+        odom.twist.twist = Twist(Vector3(self.x_k[2],self.x_k[3],0.), Vector3(0.,0.,0.))
+
+        odom_pub.publish(odom)
+
+	# Publish Raw IMU for Path Following Control
+        if self.imu is not None:
+            imu_pub = rospy.Publisher('/mur/Imu/filtered', Imu, queue_size=10)
+
+            imu_msg = self.imu
+            imu_msg.header.stamp = current_time
+            imu_msg.header.frame_id = "imu"
+            if self.x_k[3] == 0.:
+	        imu_msg.orientation = self.quaternion_from_euler(0.,0.,0.)
+            else:
+	        imu_msg.orientation = self.quaternion_from_euler(0,0,self.pi_2_pi(self.acc_k[0]))
+                imu_msg.linear_acceleration = Vector3(self.acceleration.x,self.acceleration.y,0.0)
+            imu_pub.publish(imu_msg)
+
+	
+    def imu_callback(self,imu):
+        current_time = rospy.Time.now()
+        # Initialise
+        self.imu = imu
+        self.past_acceleration_x.append(self.acceleration.x)
+        self.past_acceleration_y.append(self.acceleration.y)
+        self.past_headingimu.append(self.headingimu)
+
+        self.imudt = imu.header.stamp.to_sec() - self.imutime
+        if self.imudt>1: # Double check this!
+	    self.gpsdt=0.02
+        self.imutime = imu.header.stamp.to_sec()
+
+        # Calculate Heading (Yaw orientation)
+        w = self.imu.orientation.w
+        x = self.imu.orientation.x
+        y = self.imu.orientation.y
+        z = self.imu.orientation.z      
+
+        t0 = +2.0 * (w * x + y * z)
+       	t1 = +1.0 - 2.0 * (x * x + y * y)
+        self.rollimu = math.atan2(t0, t1)
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        self.pitchimu = math.asin(t2)
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        self.yawimu = math.atan2(t3, t4)
+        self.yawimu = self.pi_2_pi(self.yawimu)
+
+	# Zero acceleration due to gravity
+        self.acceleration.x = self.imu.linear_acceleration.x - 9.80665*sin(self.pitchimu)
+        self.acceleration.y = self.imu.linear_acceleration.y - 9.80665*sin(self.rollimu)
+        
+        self.angular_velocity = imu.angular_velocity.z
+      	self.past_imu = imu
+        self.firstimu = 0
+
+	odom_pub = rospy.Publisher('/fix/Odom', Odometry, queue_size=10)
+
+        odom = Odometry()
+        odom.header.stamp = current_time
+        odom.header.frame_id = "odom"
+
+	if self.gps is not None:
+	    odom.pose.pose = Pose(Point(self.position.x,self.position.y, 0.0), (self.imu.orientation))
+        else:
+            odom.pose.pose = Pose(Point(0.0,0.0,0.0), (self.imu.orientation))
+        odom.child_frame_id = "base_link"
+        if self.gps_velocity is not None:
+            odom.twist.twist = Twist((self.gps_velocity.vector), Vector3(0.,0.,0.))
+        else:
+            odom.twist.twist = Twist(Vector3(0.,0.,0.), Vector3(0.,0.,0.))
+
+        odom_pub.publish(odom)
+ 
+    def gps_velocity_callback(self, gps_velocity):
+        self.gps_velocity = gps_velocity
+        self.velocity = gps_velocity.vector
+
+    def gps_callback(self, gps):
+        
+        self.gps = gps
+        self.gpsdt = gps.header.stamp.to_sec() - self.gpstime
+        if self.gpsdt>5: # Double check this!
+	    self.gpsdt=1.0
+        self.gpstime = gps.header.stamp.to_sec()
+        if self.past_gps is not None:
+            if self.firstgps == 0:
+		self.firstposition = utm.from_latlon(self.past_gps.latitude,self.past_gps.longitude)
+                self.firstgps=1
+            if self.calibrated is True and self.firstgps == 1:
+                self.firstposition = utm.from_latlon(self.past_gps.latitude,self.past_gps.longitude)
+	        self.firstgps = 2
+            
+            present = utm.from_latlon(gps.latitude,gps.longitude)
+            past = utm.from_latlon(self.past_gps.latitude,self.past_gps.longitude)
+            self.dx = present[0]-past[0]
+            self.dy = present[1]-past[1]
+
+        else:
+            self.firstgps = 0
+            present = [0,0]
+            past = [0,0]
+            self.dx=0
+	    self.dy=0
+       
+	#self.position = Vector3(self.firstposition[0]-present[0],self.firstposition[1]-present[1],0.0)
+	self.position = Vector3(-(self.firstposition[1]-present[1]),self.firstposition[0]-present[0],0.0)
+        self.xk = [self.position.x,self.position.y,self.velocity.x, self.velocity.y]
+        
+        
+        # Calculate Heading (Yaw orientation)
+        self.yawgps = np.arctan2(self.position.y-self.dy, self.position.x-self.dx)
+        self.yawgps = self.pi_2_pi(self.yawgps)
+        self.past_gps = gps
+       
+        self.past_position_x.append(self.position.x)
+	self.past_position_y.append(self.position.y)
+        self.past_position_z.append(self.position.z)
+
+	
+    
+    def talker(self):
+
+        rate = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            
+            self.kalman()
+            rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('VehiclePose')
+    
+    server = Server()
+
+    rospy.Subscriber("/piksi/imu", Imu, server.imu_callback, queue_size=1)
+    rospy.Subscriber("/piksi/gps", NavSatFix, server.gps_callback, queue_size=1)
+    rospy.Subscriber("/piksi/velocity", Vector3Stamped, server.gps_velocity_callback, queue_size=1)
+
+    try:
+        server.talker()
+    except rospy.ROSInterruptException:
+        pass
+    
+    rospy.spin()
+    
+
